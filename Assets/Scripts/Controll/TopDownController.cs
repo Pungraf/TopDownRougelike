8f4ea545163fcaffbfd26d369c9e3f802ffde8ba
Jan Bartłomiejczyk <44816218+Pungraf@users.noreply.github.com>
2022-11-27 17:02:00 +0100

    Camera Controller, Running Animation Blend

--- Assets/Scripts/Controll/TopDownController.cs
+++ Assets/Scripts/Controll/TopDownController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class TopDownController : MonoBehaviour
 {
     private InputHandler _input;
+    private Animator _animator;
 
     [SerializeField]
     private float moveSpeed;
@@ -13,27 +14,36 @@ public class TopDownController : MonoBehaviour
     private float rotateSpeed;
     [SerializeField]
     private Camera camera;
+    [SerializeField]
+    LayerMask _aimLayerMask;
 
     private void Awake()
     {
         _input = GetComponent<InputHandler>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
-
-        var MovementVector = MoveTowardTarget(targetVector);
-
+        //Moving
+        MoveTowardTarget(targetVector);
+        //Rotating
         RotateTowardMouseVector();
+        //Animating
+        float velozityX = Vector3.Dot(targetVector.normalized, transform.right);
+        float velozityZ = Vector3.Dot(targetVector.normalized, transform.forward);
+
+        _animator.SetFloat("VelocityX", velozityX, 0.1f, Time.deltaTime);
+        _animator.SetFloat("VelocityZ", velozityZ, 0.1f, Time.deltaTime);
     }
 
     private void RotateTowardMouseVector()
     {
         Ray ray = camera.ScreenPointToRay(_input.MousePosition);
 
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f, _aimLayerMask))
         {
             var target = hitInfo.point;
             target.y = transform.position.y;
@@ -41,14 +51,13 @@ public class TopDownController : MonoBehaviour
         }
     }
 
-    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    private void MoveTowardTarget(Vector3 targetVector)
     {
         var speed = moveSpeed * Time.deltaTime;
 
         targetVector = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y, 0) * targetVector ;
         var targetPosition = transform.position + targetVector * speed;
         transform.position = targetPosition;
-        return targetVector;
     }
 
 }
